5ee142c4152a52cb99c09cfc63eb100f72be3a1f
Timóteo Graebin <timotyduo@gmail.com>
2025-08-03 19:03:24 -0300

    Create plugin.go

---
+++ plugin.go
@@ -0,0 +1,16 @@
+package swaggoit
+
+func Start(configsProject, configsPath struct) {
+  fmt.Println("Hello World")
+
+  fmt.Print(`
+---------------
+`)
+  fmt.Println(configsProject)
+
+  fmt.Print(`
+---------------
+`)
+
+  fmt.Println(configsPath)
+}
